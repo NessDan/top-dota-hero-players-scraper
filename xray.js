db51e93dbf6dd8fe7583cc417a9118c3907664d2
Daniel Kaspo <danielkaspo@gmail.com>
2016-04-03 23:14:20 -0400

    Almost works, loop needs to be reworked to synch.

---
+++ xray.js
@@ -0,0 +1,79 @@
+var Xray = require('x-ray');
+var x = Xray();
+var fs = require('fs');
+
+// Global config
+var playersPerHero = 15;
+var matchesPerPlayer = 15;
+
+function getAllHeroes() {
+	var query = {
+		"url": "http://www.dotabuff.com/heroes",
+		"scope": ".hero-grid",
+		"selector": ["a@href"],
+	};
+
+	x(query.url, query.scope, query.selector)(function(err, response) {
+		var heroUrls = response;
+
+		if (heroUrls.length > 100) {
+			getTopPlayers(heroUrls);
+		} else {
+			console.error('problem with response.');
+		}
+	});
+}
+
+function getTopPlayers(heroUrls) {
+	for (var i = 0; i < heroUrls.length; i++) {
+		var heroUrl = heroUrls[i];
+
+		var query = {
+			"url": heroUrl + "/players",
+			"scope": "table.sortable",
+			"selector": ["tr@data-link-to"],
+		}
+
+		x(query.url, query.scope, query.selector)(function(err, response) {
+			response.splice(0, 1); // (remove useless value picked up.)
+			getPlayerMatches(response);
+			// console.log('second', response);
+		});
+	}
+}
+
+function getPlayerMatches(playersMatches) {
+	for (var i = 0; i < playersPerHero; i++) {
+		playerMatches = playersMatches[i];
+
+		var query = {
+			"url": "http://www.dotabuff.com" + playerMatches,
+			"scope": "td.cell-large",
+			"selector": ["a@href"],
+		}
+
+		x(query.url, query.scope, query.selector)(function(err, response) {
+			var matchUrls = response;
+
+			if (!matchUrls) {
+				return;
+			}
+
+			for (var j = 0; j < matchesPerPlayer; j++) {
+				var matchUrl = matchUrls[j];
+				var matchId = matchUrl.split("matches/").pop().trim();
+				fs.appendFileSync("./top-player-match-ids.txt", matchId + "\r\n");
+			}
+
+			// console.log('third', response);
+		});
+	}
+
+}
+
+function main() {
+	fs.writeFileSync("./top-player-match-ids.txt", ""); // clear the match ids
+	getAllHeroes();
+}
+
+main();
